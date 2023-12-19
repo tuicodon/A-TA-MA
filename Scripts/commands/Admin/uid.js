2c8836029c13292f80f63969e014124f9d6ea7a7
tuicodon <154343207+tuicodon@users.noreply.github.com>
2023-12-20 05:22:37 +0700

    Create uid.js

---
+++ Scripts/commands/Admin/uid.js
@@ -0,0 +1,17 @@
+const config = {
+    credits: "XaviaTeam",
+  isAbsolute: true
+  
+}
+
+function onCall({ message }) {
+    const { senderID, mentions, reply, type } = message;
+    const msg = Object.keys(mentions).length == 0 ? type == "message_reply" ? message.messageReply.senderID : senderID : Object.entries(mentions).map(e => `${e[1].replace(/@/g, '')} - ${e[0]}`).join("\n");
+
+    reply(msg);
+}
+
+export default {
+    config,
+    onCall
+}
